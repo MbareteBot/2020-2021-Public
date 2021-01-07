793bf52462fed4f5bc242a673c5ddbd9dadcf324
maino <66177873+MainoGimenez@users.noreply.github.com>
2021-01-07 01:52:25 -0300

    Fixed: error when pushing the new src folder for Scorer App

---
+++ Scorer App/src/components/Mission.js
@@ -0,0 +1,189 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Switch, Image } from "react-native";
+import {Picker} from '@react-native-picker/picker';
+import CText from "./CustomText"
+
+
+export default function Mission(props) {
+
+  /*
+    Default args: {imgSource, name, description, counterHandler, points}
+    options args: {[["name1", "name2", "etc"], [pointname1, pointname2, etc]]} 
+  */
+  const [pointsAddedByOptions, setPointsAddedByOptions] = useState(0);
+  const [isMissionEnabled, setIsMissionEnabled] = useState(false);
+  const [isEnabled, setIsEnabled] = useState([]);
+  const [pickerValue, setPickerValue] = useState();
+  const toggleMissionSwitch = () => setIsMissionEnabled(prevState => {
+    if (props.state != undefined) props.state(!prevState)
+    if (prevState) {
+      setPointsAddedByOptions(0)
+      setIsEnabled([])
+    }
+    return !prevState
+  });
+  
+  const handleCounter = (state, counterHandler, points) => {
+    if (state) counterHandler(prevPoints => prevPoints + points)
+    else { 
+      counterHandler(prevPoints => ((prevPoints - points) - pointsAddedByOptions))
+    }
+  }
+  const toggleSwitch = (index) => setIsEnabled(prevStates => {
+    if (prevStates.length == 0) for (let i = 0; i < props.options[0].length; i++) prevStates.push(false)
+    var updatedStates = prevStates;
+    var state = 0;
+    for (state in updatedStates) {
+      if (state == index) updatedStates[index] = !updatedStates[index]
+    }
+    return updatedStates
+  });
+  const handleCounterOptions = (state, counterHandler, points) => {
+    if (state) {
+      counterHandler(prevPoints => prevPoints + points);
+      setPointsAddedByOptions(prevState => prevState + points)
+    }
+    else {
+      counterHandler(prevPoints => prevPoints - points)    
+      setPointsAddedByOptions(prevState => prevState - points)
+    }
+  }
+
+  const [pointsAddedByPicker, setPointsAddedByPicker] = useState(0)
+  const [pickerLastValue, setPickerLastValue] = useState("NaN")
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.options}>
+        <View style={styles.missionInfo}>
+          { props.imgSource != undefined ? (
+              <View style={styles.missionImg}>
+                <Image
+                source={props.imgSource}
+                style={{width: 40, height: 40}}
+                />
+              </View>
+            ) : null }
+            <CText style={{fontSize: 17}}>{props.name}</CText>
+        </View>
+        <Switch
+          trackColor={{ false: "gray", true: "lightblue" }}
+          thumbColor={"white"}
+          style={{flex: 1}}
+          ios_backgroundColor={"#3e3e3e"}
+          value={isMissionEnabled}
+          onValueChange={(switchState) => {
+            toggleMissionSwitch();
+            handleCounter(switchState, props.counterHandler, props.points);
+          }} />
+      </View>
+
+      
+      { isMissionEnabled ? (
+        <View style={styles.missionDescription}>
+          <CText>{props.description}</CText>
+        </View> ) : null }
+
+
+        { props.pickerOptions && isMissionEnabled ? (
+            <View style={styles.picker} key={Math.random()}>
+              <Picker
+                selectedValue={pickerValue}
+                onValueChange={(value) => {
+                  setPointsAddedByPicker(value)
+                  if (pickerLastValue != "NaN") {
+                    handleCounter(false, props.counterHandler, pickerLastValue)
+                  }
+                  handleCounter(true, props.counterHandler, value)
+                  setPickerLastValue(value)
+                  setPickerValue(value)
+                  }} >
+                {props.pickerOptions[0].map((val, index) => 
+                  <Picker.Item 
+                    label={val} 
+                    value={props.pickerOptions[1][index]} 
+                    color={props.pickerOptions[2][index]} />
+                )}
+              </Picker>
+            </View>
+        ): null }  
+
+        
+      { props.options && isMissionEnabled ? (
+        props.options[0].map((value, index) => 
+          <View style={styles.options} key={index.toString()}>
+            <View style={styles.optionDescription}>
+            <CText>{value}</CText>
+            </View>
+            <View>
+              <Switch
+                trackColor={{ false: "gray", true: "lightblue" }}
+                thumbColor={"white"}
+                ios_backgroundColor={"#3e3e3e"}
+                value={isEnabled[index]}
+                onValueChange={(switchState) => {
+                  toggleSwitch(index);
+                  handleCounterOptions(switchState, props.counterHandler, props.options[1][index]);
+                }} />
+              </View>
+          </View>  
+        )) : null } 
+
+        { isMissionEnabled ? (
+          <View style={styles.missionPoints}>
+            <CText>Points: {props.points + pointsAddedByOptions + pointsAddedByPicker}</CText>
+          </View>
+        ) : null}      
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 8,
+    paddingHorizontal: 25,
+    paddingTop: 10,
+    paddingBottom: 20,
+    margin: 10,
+    marginVertical: 4,
+    backgroundColor: "#424C61",
+  },
+  missionInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#424C61",
+    flex: 2
+  },
+  missionImg: {
+    paddingRight: 10,
+    marginRight: 10,
+    borderRightWidth: 1,
+    borderRightColor: "rgba(255,255,255,0.3)"
+  },
+  missionDescription: {
+    alignItems: "center",
+    marginTop: 10
+  },
+  options: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  optionDescription: {
+    flex: 1
+  },
+  missionPoints: {
+    alignItems: "center",
+    marginTop: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "white"
+  },
+  picker: {
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    marginTop: 10
+  },
+
+})
