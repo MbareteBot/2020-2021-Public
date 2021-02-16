c0df6379c165ef4b0e56c3d9b8519fd986aa04ad
maino <66177873+MainoGimenez@users.noreply.github.com>
2021-02-16 20:14:01 -0300

    removed junk files

--- Testing/code_for_robot.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env pybricks-micropython
-from pybricks.ev3devices import Motor
-from pybricks.parameters import Port
-
-motor = Motor(Port.A)
-while True:
-    command = input("$: ")
-    if command == "w":
-        motor.run_time(100, 500)
