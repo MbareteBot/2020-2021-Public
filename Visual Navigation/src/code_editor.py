855ea2315a67fb278d853ed1669713c958021d9a
maino <66177873+mainow@users.noreply.github.com>
2021-03-28 14:43:27 -0400

    Deleted App and VN folders

--- Visual Navigation/src/code_editor.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-
-class CodeEditorWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        self.setGeometry(0, 0, 100, 100)
-        self.show()
-        print("NEW")
